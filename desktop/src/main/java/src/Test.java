e392d0e17dd933ff73f6f8f4eb3f48d214087017
kdbogusz <konrad_bogusz@wp.pl>
2021-04-30 13:45:21 +0200

    early plot script

---
+++ desktop/src/main/java/src/Test.java
@@ -0,0 +1,17 @@
+package src;
+
+public class Test {
+    public static void main(String[] args){
+        try {
+            String[] script = {"python", "C:\\Users\\nuttard\\repos\\Colorimeter\\desktop\\src\\main\\java\\src\\plot.py", "0.2", "0.2", "0.1", "0.1"};
+            Runtime.getRuntime().exec(script);
+//            ProcessBuilder pb = new ProcessBuilder(script);
+//            ProcessBuilder pb = new ProcessBuilder("python", "C:\\Users\\nuttard\\repos\\Colorimeter\\desktop\\src\\main\\java\\src\\plot.py", "0.3", "0.3", "0.1", "0.1");
+//            Process p = pb.start(); // Start the process.
+//            p.waitFor(); // Wait for the process to finish.
+//            System.out.println("Script executed successfully");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
