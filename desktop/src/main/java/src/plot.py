ffcfd46b3fd7f09d2aef350e86ab44b0ff7bacfc
kdbogusz <konrad_bogusz@wp.pl>
2021-05-07 11:18:50 +0200

    graph improvements

--- desktop/src/main/java/src/plot.py
+++ desktop/src/main/java/src/plot.py
@@ -3,35 +3,43 @@ import colour
 import matplotlib.pyplot as plt
 from colour.plotting import plot_sds_in_chromaticity_diagram_CIE1931, plot_chromaticity_diagram_CIE1931
 import numpy as np
+from matplotlib.patches import FancyArrow, FancyArrowPatch
 
 
 def draw_plot(xs, ys, dxs, dys, path):
     plot_chromaticity_diagram_CIE1931(standalone=False)
 
+    arrows = []
     for i in range(len(xs)):
         x, y = xs[i], ys[i]
         xy = (x, y)
         plt.plot(x, y, 'o-', color='white')
-        patch_name = "original-" + str(i + 1)
-        plt.annotate(patch_name,
-                     xy=xy,
-                     xytext=(-50, 30),
-                     textcoords='offset points',
-                     arrowprops=dict(arrowstyle='->', connectionstyle='arc3, rad=-0.2'))
+        # patch_name = "original-" + str(i + 1)
+        # plt.annotate(patch_name,
+        #              xy=xy,
+        #              xytext=(-50, 30),
+        #              textcoords='offset points',
+        #              arrowprops=dict(arrowstyle='->', connectionstyle='arc3, rad=-0.2'))
 
 
         x, y = xs[i] + dxs[i], ys[i] + dys[i]
         xy = (x, y)
         plt.plot(x, y, 'o-', color='black')
-        patch_name = "measured-" + str(i + 1)
-        plt.annotate(patch_name,
-                     xy=xy,
-                     xytext=(-50, 30),
-                     textcoords='offset points',
-                     arrowprops=dict(arrowstyle='->', connectionstyle='arc3, rad=-0.2'))
+        # patch_name = "measured-" + str(i + 1)
+        # plt.annotate(patch_name,
+        #              xy=xy,
+        #              xytext=(-50, 30),
+        #              textcoords='offset points',
+        #              arrowprops=dict(arrowstyle='->', connectionstyle='arc3, rad=-0.2'))
 
+        plt.annotate("", xy=(xs[i] + dxs[i], ys[i] + dys[i]), xytext=(xs[i], ys[i]), arrowprops=dict(facecolor='black', arrowstyle='->'))
+
+    plt.title("")
     fig = plt.gcf()
+    fig.suptitle("")
     fig.set_size_inches(5, 5)
+    for arrow in arrows:
+        fig.add_artist(arrow)
     fig.savefig(path + "\\plot.png", dpi=96)
 
 
